ac6077c17d271e5ac0a7ba95450c526f637652e3
ThisisAtharv <atharv.kurlapkar47@gmail.com>
2023-09-10 11:40:04 +0530

    'Pure Function' File added.

---
+++ Pure Function.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// In Solidity, a function that doesn’t read or modify the variables of the state is called a pure function. 
+// It can only use local variables that are declared in the function and the arguments that are passed to the function to 
+// compute or return a value.
+
+contract PureExample {
+    uint number1 = 6;
+    function getNum(uint number2) public pure returns(uint) {
+    //number1 = number2;            // If these 2 lines are uncommented, compiler produces error because of 'pure' keyword.
+    //return number1;
+  }
+
+  // Explanation:
+  // 1) We declare a static variable number1.
+  // 2) We define a view function named getNum().
+  // 3) getNum() takes a number and tries to modify static variable using it which will produce error since pure functions do not allow
+  // to read and write these variables.
+}
