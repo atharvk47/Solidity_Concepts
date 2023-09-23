b7dffb79b385959540dbdb7321782b50902c5631
ThisisAtharv <atharv.kurlapkar47@gmail.com>
2023-09-23 23:57:38 +0530

    File modified.

---
+++ Try Catch.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract WillThrow {
+    function aFunction() public pure {
+        require(false, "Error message");
+    }
+}
+
+contract ErrorHandling {
+    event ErrorLogging(string reason);
+    function catchError() public {
+        WillThrow will = new WillThrow();
+        try will.aFunction() {
+            //here we could do something if it works
+        }  catch Error(string memory reason) {
+            emit ErrorLogging(reason);
+        }
+    }
+}
